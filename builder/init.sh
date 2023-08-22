cf6db8ccfb27616e0c6bfd6b2c9452b72d055085
Sylvain Prat <sylvain.prat@achird.net>
2023-08-22 20:12:14 +0200

    Improve the init image

--- builder/init.sh
+++ builder/init.sh
@@ -1,4 +1,4 @@
-#!/bin/busybox sh
+#!/busybox sh
 # shellcheck shell=dash
 set -eu
 
@@ -7,7 +7,7 @@ exec >/dev/console 2>/dev/console
 
 # Some programs (e.g. containerd) refuse to work if / is a "rootfs" mount, as in an initramfs.
 # So, we need to copy all the files into a new tmpfs and "switch_root" to it
-b=/bin/busybox
+b=/busybox
 $b mkdir -p /newroot
 $b mount -t tmpfs root /newroot
 $b find /system -mindepth 1 -maxdepth 1 -exec $b mv -t /newroot {} +
