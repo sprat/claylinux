a5d895ea081539b7c4b4cd8945b2130b85434279
Sylvain Prat <sylvain.prat@achird.net>
2025-04-15 23:51:58 +0200

    Support all the images formats in the emulator

--- emulator.sh
+++ emulator.sh
@@ -14,6 +14,15 @@ case "$FORMAT" in
       qcow2)
             boot_opts=(-drive "file=${name}.qcow2,format=qcow2")
             ;;
+      vmdk)
+            boot_opts=(-drive "file=${name}.vmdk,format=vmdk")
+            ;;
+      vhdx)
+            boot_opts=(-drive "file=${name}.vhdx,format=vhdx")
+            ;;
+      vdi)
+            boot_opts=(-drive "file=${name}.vdi,format=vdi")
+            ;;
       iso)
             boot_opts=(-drive "file=${name}.iso,media=cdrom")
             ;;
